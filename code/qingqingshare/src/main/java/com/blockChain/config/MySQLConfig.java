8f69ea3ed63d3e0fa26af2c2270f766bc0216009
allerH <1095833846@qq.com>
2019-08-29 22:09:30 +0800

    更新了wrapper

---
+++ code/qingqingshare/src/main/java/com/blockChain/config/MySQLConfig.java
@@ -0,0 +1,37 @@
+package com.blockChain.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource(value = "classpath:config/mysql.properties")
+public class MySQLConfig {
+
+    @Value(value="${blockchain.mysql.jdbc.url}")
+    private String jdbcUrl;
+
+    @Value(value="${blockchain.mysql.jdbc.user}")
+    private String jdbcUser;
+
+    @Value(value = "${blockchain.mysql.jdbc.password}")
+    private String jdbcPassword;
+
+    @Value(value = "${blockchain.mysql.jdbc.driver}")
+    private String jdbcDriver;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource=new DriverManagerDataSource();
+        dataSource.setDriverClassName(jdbcDriver);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(jdbcUser);
+        dataSource.setPassword(jdbcPassword);
+        return dataSource;
+    }
+}
---
+++ code/qingqingshare/src/main/java/com/blockChain/config/SqlSessionConfig.java
@@ -0,0 +1,53 @@
+package com.blockChain.config;
+
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class SqlSessionConfig implements TransactionManagementConfigurer{
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(){
+        SqlSessionFactoryBean bean=new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setTypeAliasesPackage("com.blockChain.dao.domin");
+
+        ResourcePatternResolver resolver=new PathMatchingResourcePatternResolver();
+        try{
+            bean.setMapperLocations(resolver.getResources("classpath*:META-INF/mybatis/*Mapper"));
+            return bean.getObject();
+        }catch (Exception e){
+            throw new RuntimeException();
+        }
+    }
+
+    @Bean
+    public PlatformTransactionManager txManager(){
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    /**
+     * @see
+     */
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager(){
+        return txManager();
+    }
+}
