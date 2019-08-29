6eb68cd18c6574d567d12ac7a77fe7bf03178368
allerH <1095833846@qq.com>
2019-08-29 22:00:54 +0800

    后端架构

--- code/qingqingshare/src/main/java/com/blockChain/config/MapperScannerConfig.java
+++ code/qingqingshare/src/main/java/com/blockChain/config/MapperScannerConfig.java
@@ -1,8 +1,21 @@
 package com.blockChain.config;
 
 
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@AutoConfigureAfter(SqlSessionConfig.class)
 public class MapperScannerConfig {
 
-
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer(){
+        MapperScannerConfigurer mapperScannerConfigurer=new MapperScannerConfigurer();
+        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        mapperScannerConfigurer.setBasePackage("com.blockChain.dao.mapper");
+        return mapperScannerConfigurer;
+    }
 }
