960d04a2e5a22f47cae5ec586f9520c221338278
Four5Fire <942558046@qq.com>
2019-09-01 11:26:58 +0800

    编写用户模块

--- code/qingqingshare/src/main/java/com/blockChain/dao/UserDAO.java
+++ code/qingqingshare/src/main/java/com/blockChain/dao/UserDAO.java
@@ -1,8 +1,11 @@
 package com.blockChain.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserDAO {
-
+public interface UserDAO {
+    int selectUserByUserName(@Param("username") String username);
 }
