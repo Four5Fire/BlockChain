5fcaa9f9ab7765a454954f529d74d4e4473647c7
SheraLU <luxijun@outlook.com>
2019-09-12 01:10:43 +0800

    修改测试用例描述

---
+++ code/qingqingshare/src/main/java/com/blockChain/Test.java
@@ -0,0 +1,11 @@
+package com.blockChain;
+
+import com.blockChain.Cipher.AES.Aes;
+import com.blockChain.util.IPFSUtils;
+import com.blockChain.util.MD5Util;
+
+public class Test {
+    public static void main(String[] args) {
+        IPFSUtils.getFile("111");
+    }
+}
